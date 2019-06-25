e32ab035c9e0ce6961290c381fb8a13084371b95
kntzn <kntznart@gmail.com>
2019-06-25 04:35:04 +0300

    [Add]: function template [Add]: printOut

--- clusterizer/clusterizer/main.cpp
+++ clusterizer/clusterizer/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+#define M_SIZE 10
 
 struct Obj
     {
@@ -15,7 +16,18 @@ struct Obj
         { }
     };
 
-template <typename T> void function (std::vector <T> obj);
+template <typename T> void function (std::vector <T> obj, double d, double (*metrics) (T, T));
+
+
+double distance (Obj obj0, Obj obj1)
+    {
+    double dx = obj0.x - obj1.x;
+    double dy = obj0.y - obj1.y;
+
+    return sqrt (dx*dx + dy*dy);
+    }
+
+void printOut (std::vector <Obj> objects);
 
 int main ()
     { 
@@ -23,23 +35,35 @@ int main ()
 
     std::vector <Obj> objects;
     for (int i = 0; i < nObj; i++)
-        objects.push_back (Obj (rand() % 10, rand() % 10));
+        objects.push_back (Obj (rand() % M_SIZE, rand() % M_SIZE));
     
+    function (objects, 3, distance);
 
     system ("pause");
     return 0;
     }
 
 
-double metrics (Obj obj0, Obj obj1)
-    { 
-    double dx = obj0.x - obj1.x;
-    double dy = obj0.y - obj1.y;
+template<typename T>
+void function (std::vector<T> obj, double d, double (*metrics)(T, T))
+    {
 
-    return sqrt (dx*dx + dy*dy);
     }
 
-template<typename T>
-void function (std::vector<T> obj)
-    {
+void printOut (std::vector <Obj> objects)
+    { 
+    int matrix [M_SIZE] [M_SIZE] = { };
+
+    for (auto i : objects)
+        matrix [(int) i.x] [(int) i.y]++;
+
+    printf ("\n");
+    for (int i = 0; i < 10; i++)
+        {
+        for (int j = 0; j < 10; j++)
+            {
+            printf ("%d", matrix [i] [j]);
+            }
+        printf ("\n");
+        }
     }
