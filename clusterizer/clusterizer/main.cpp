97435ce3e1ba06c1c58024b9fef1803b3c3c8b02
kntzn <kntznart@gmail.com>
2019-07-03 17:19:45 +0300

    [Add]: cluster collector

--- clusterizer/clusterizer/main.cpp
+++ clusterizer/clusterizer/main.cpp
@@ -44,16 +44,70 @@ int main ()
 template<typename T>
 std::vector<std::vector <T>> DBSCAN (std::vector<T> obj, double d, double n, double (*metrics)(T, T))
     {
-    std::vector <int> nNeighb;
+    // List of the neighbours for each object
+    std::vector <std::vector <int>> neighb;
     
-    // Counts number of neighbours for each object 
+    // Collects ids of the neighbours for each object
     for (int i = 0; i < obj.size (); i++)
         {
-        nNeighb.push_back (0);
+        neighb.push_back (std::vector <int>);
+
         for (int j = 0; j < obj.size (); j++)
             if (i != j && metrics (obj [i], obj [j]) < d)
-                nNeighb [i]++;
+                neighb [i].push_back (j);
         }
 
+    std::vector <int> cluster_ids (obj.size ()) = { };
     
+    int n_clusters = 0;
+
+    // This creates new clusters
+    for (int i = 0; i < obj.size (); i++)
+        {
+        int nNeighb = neighb [i].size ();
+        
+        // In case this object has enough neighb.
+        if (nNeighb >= n)
+            { 
+            // For each nieghbour
+            for (int j = 0; j < nNeighb; j++)
+                {
+                // "object j" id
+                int this_neighb_id = neighb [i][j];
+
+                // if "object j" is already in cluster
+                if (cluster_ids [obj [this_neighb_id]] != 0)
+                    // Join "object i" to the same cluster
+                    cluster_ids [i] = cluster_ids [obj [this_neighb_id]];
+                // else 
+                else
+                    {
+                    // if "object j" has no cluster
+                    if (cluster_ids [i] == 0)
+                        // Create new cluster with both objects
+                        cluster_ids [i] = cluster_ids [obj [this_neighb_id]] = ++n_clusters;
+                    else 
+                        // Else (if "object i" was clusterized before) join "object j" to the same cluster
+                        cluster_ids [obj [this_neighb_id]] = cluster_ids [i];
+                    }
+
+
+                }
+            }
+        }
+
+
+    //// Joins to other clusters
+    //else if (nNeighb == n - 1)
+    //    { }
+    //// Don't join
+    //else
+    //    { }
+
+    // Convert clusters_ids to the clusters object
+
+    std::vector <std::vector <T>> clusters;
+
+
+
     }
